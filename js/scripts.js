b0a22429e32653bc4fc772efa3e520d72d0570f5
Jay Whang and Taylor Grohs <taylorgrohs@gmail.com>
2016-01-25 09:22:08 -0800

    Fix clickable li

--- js/scripts.js
+++ js/scripts.js
@@ -14,12 +14,12 @@ $(document).ready(function(){
     var inputtedLastName = $("input#new-last-name").val();
     var newContact = new Contact(inputtedFirstName, inputtedLastName);
 
-    $("ul#contacts").append("<li><span class='contact'>" + newContact.fullName() + "</span></li>");
+    $("ul#contacts").append("<li><span class='contacts'>" + newContact.fullName() + "</span></li>");
 
     $("input#new-first-name").val("");
     $("input#new-last-name").val("");
 
-    $("#contacts").last().click(function(){
+    $(".contacts").last().click(function(){
       $("#show-contact").show();
       $("#show-contact h2").text(newContact.fullName());
       $(".first-name").text(newContact.firstName);

477f1b5e98fb8d0fceb686827fdb10ad47415852
Jay Whang and Michael Schennum <mikeschenphoto@gmail.com>
2016-01-26 08:19:50 -0800

    add Address function

--- spec/specs.js
+++ spec/specs.js
@@ -8,6 +8,15 @@ describe('Contact', function() {
 
   it("adds the fullName method to all contacts", function(){
     var testContact = new Contact("Dead", "Pool");
-    expect(testContact.fullName()).to.equal("Dead Pool");   
+    expect(testContact.fullName()).to.equal("Dead Pool");
+  });
+
+});
+describe('Address', function() {
+  it("creates a new address with the given specifications", function() {
+    var testAddress = new Address("123 Main St", "Test City","Test State");
+    expect(testAddress.street).to.equal("123 Main St");
+    expect(testAddress.city).to.equal("Test City");
+    expect(testAddress.state).to.equal("Test State");
   });
 });
